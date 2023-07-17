bed1c10ecb15708406247523e051627a9e38f4f4
Inazumi Satonao <fnherstal.five.seven@gmail.com>
2023-07-17 12:43:23 +0000

    2.5

---
+++ routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  get 'topics:/new', to: 'topics#new'
+  get 'topics/edit', to: 'topics#edit'
+end
